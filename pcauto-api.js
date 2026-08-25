17b393545ad4849b3ec09099e2401c486b5826c8
Phamcuong1405 <cuongkool1994@gmail.com>
2026-08-25 20:46:42 +0700

    chore: cap nhat URL API PCAUTO login

---
+++ pcauto-api.js
@@ -0,0 +1,3 @@
+/* PCAUTO â€” API cÃ´ng khai (Cloudflare tunnel cá»•ng 4000). File nÃ y Ä‘Æ°á»£c cáº­p nháº­t khi má»Ÿ app. */
+window.PCAUTO_API_BASE = 'https://bride-arch-reporter-achieved.trycloudflare.com/api/v1';
+window.PCAUTO_APP_URL = 'http://localhost:3000';
